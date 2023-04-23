e720b6f191a3f325c954fa794408b7f98c6fc914
santiagomd11 <vs.montano@uniandes.edu.co>
2023-04-22 20:41:43 -0500

    Adding 1000x1000 map in tests just to see the result

--- tests/test_path.py
+++ tests/test_path.py
@@ -18,3 +18,11 @@ class PathTestCase(unittest.TestCase):
         print("output ======> ")
         print(f'length: {path["length"]}\npath: {path["path"]}\ndrop: {path["drop"]}')
         self.assertEqual(path, {"length": 5, "path": [9, 5, 3, 2, 1], "drop": 8})
+    
+    def test_last_map(self):
+        print('--------------- Testing 1000x1000 map-----------------------')
+        self.map_path = './data/map.txt'
+        map_size, map = self.utils.parse_data(self.map_path)
+        path = Path(map_size, map).get_path()
+        print("output ======> ")
+        print(f'length: {path["length"]}\npath: {path["path"]}\ndrop: {path["drop"]}')
