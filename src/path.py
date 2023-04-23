53314ae5780e28022a252e470e5e44dc00574767
santiagomd11 <vs.montano@uniandes.edu.co>
2023-04-22 20:33:03 -0500

    Adding logic 4x4 case OK

--- src/path.py
+++ src/path.py
@@ -2,10 +2,44 @@ import numpy as np
 
 class Path:
     def __init__(self, map_size, map):
-        self.__output_data = {}
+        self.__output_data = {"length": 0, "path": [], "drop": 0}
         self.__map_size = map_size
         self.__map = map
         
     def get_path(self) -> dict:      
-        print(self.__map)
+        longest_path = []
+        m, n = self.__map_size
+        max_drop = -np.inf
+        
+        for i in range(m):
+            for j in range(n):
+                path = self.deep_first_search(i, j, [])
+                steep = path[0] - path[-1]
+                if len(path) >= len(longest_path):
+                    if steep > max_drop:
+                        longest_path = path
+                        max_drop = steep
+        
+        self.__output_data["length"] = len(longest_path)
+        self.__output_data["path"] = longest_path
+        self.__output_data["drop"] = max_drop
         return self.__output_data 
+    
+    def deep_first_search(self, i: int, j: int, path: list) -> list:        
+        # First check to see if cell is out of bounds
+        cell_out_of_bounds = i >= self.__map_size[0] or j >= self.__map_size[1]
+        
+        # Check if it's not decreasing
+        not_decreasing = cell_out_of_bounds or \
+                        (path and self.__map[i][j] >= path[-1])
+                        
+        if not_decreasing:
+            return path
+        
+        # Adding map[i, j] to the path and apply the  same logic recursively in all directions
+        up = self.deep_first_search(i - 1, j, path + [self.__map[(i, j)]])
+        down = self.deep_first_search(i + 1, j, path + [self.__map[(i, j)]])
+        right = self.deep_first_search(i, j + 1, path + [self.__map[(i, j)]])
+        left = self.deep_first_search(i, j - 1, path + [self.__map[(i, j)]])
+        
+        return max(up, down, right, left, key=len)
--- tests/test_path.py
+++ tests/test_path.py
@@ -11,7 +11,10 @@ class PathTestCase(unittest.TestCase):
         self.map_path = ''
     
     def test_path_first_map(self):
+        print('--------------- Testing 4x4 map-----------------------')
         self.map_path = './data/4x4.txt'
         map_size, map = self.utils.parse_data(self.map_path)
         path = Path(map_size, map).get_path()
-        self.assertEqual(path, {"length:": 5, "path": [9, 5, 3, 2, 1], "drop": 8})
+        print("output ======> ")
+        print(f'length: {path["length"]}\npath: {path["path"]}\ndrop: {path["drop"]}')
+        self.assertEqual(path, {"length": 5, "path": [9, 5, 3, 2, 1], "drop": 8})
